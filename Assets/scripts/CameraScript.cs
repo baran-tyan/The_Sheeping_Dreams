3b896186c39fc15143320a94253097cf8763488a
baran-tyan <mary312@ukr.net>
2017-12-09 21:21:38 +0200

    Initial commit

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+
+    public GameObject character;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(character.transform.position.x, character.transform.position.y, -10f);
+	}
+}
---
+++ Assets/scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public CharacterMoovment Player;
+
+    private void Start()
+    {
+
+        Player = Player == null ? GetComponent<CharacterMoovment>() : Player;
+        if (Player == null)
+        {
+            Debug.LogError("Player not set to controller");
+        }
+    }
+
+    private void Update()
+    {
+
+        if (Player != null)
+        {
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                Player.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Player.MoveLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Player.Jump();
+            }
+        }
+    }
+}
